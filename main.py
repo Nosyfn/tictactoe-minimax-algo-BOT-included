856993f9911315fe6a2391d16655ad87c2f2d524
sai121663 <saithanpanchaharan123@gmail.com>
2025-05-03 14:20:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,12 @@ class board:
   def __init__(self, board=[[',', ',', ','], [',', ',', ','], [',', ',', ',']], p1: str, p2: str):
     self.board = board
     self.player_1 = p1
-    
     self.player_2 = p2
     self.p1_score = 0
     self.p2_score = 0
   def print_board(self, board):
     result = ""
-    for row in board: 
+    for row in board:
       result += '|'.join(row) + /n
     return result
-  
+
