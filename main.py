39f53b165dd694adb9d564618033f6edccad4889
Nosyfn <sahibnijjar1@gmail.com>
2025-05-03 14:26:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-class board:
+class tictactoe:
+    board: list[list[str]]
+    player_1: str
+    player_2: str
+    p1_score: int
+    p2_score: int
+    def __init__(self, p1: str, p2: str):
+      self.board = [[',', ',', ','], [',', ',', ','], [',', ',', ',']]
+      self.player_1 = p1
+      self.player_2 = p2
+      self.p1_score = 0
+      self.p2_score = 0
+    def print_board(self, board):
+      result = ""
+      for row in board:
+        result += '|'.join(row) + '\n'
+      return result
 
-  board: list[list[str]]
-  player_1: str
-  player_2: str
-  p1_score: int
-  p2_score: int
-  def __init__(self, board=[[',', ',', ','], [',', ',', ','], [',', ',', ',']], p1: str, p2: str):
-    self.board = board
-    self.player_1 = p1
-    self.player_2 = p2
-    self.p1_score = 0
-    self.p2_score = 0
-  def print_board(self, board):
-    result = ""
-    for row in board:
-      result += '|'.join(row) + /n
-    return result
+    # def check_winner(self, board):
+
 
