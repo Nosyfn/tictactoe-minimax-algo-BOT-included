4905e561f41b8d88beac41774fc4fa20533f34fa
Nosyfn <sahibnijjar1@gmail.com>
2025-05-04 23:00:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import random
 class Tictactoe:
     board: list[list[str]]
     player_1: list
@@ -10,11 +10,9 @@ class Tictactoe:
         self.player_2 = [p2, 0]
         self.player_turn = 0
         # if self.player_move is even, it is player 1 turn so use X, else 0
-
-
-    def print_board(self, board):
+    def print_board(self):
         result = ""
-        for row in board:
+        for row in self.board:
             result += '|'.join(row) + '\n'
         return result
 
@@ -71,7 +69,6 @@ class Tictactoe:
 
         return f'The winner is {winner} with {winner[1]} points, beating {second} with {second[1]} points'
 
-
     def player_move(self, row, column):
         """Take user input for move and update board"""
 
@@ -84,17 +81,78 @@ class Tictactoe:
         # if self.check_winner(self.board):
         #     winner = self.player_1[0] if self.player_turn % 2 == 0 else self.player_2[0]
         #     return f'{winner} has won this round'
+    def check_if_moves(self):
+        """check if there are still moves"""
+        for i in self.board:
+            if ',' in i:
+                return True
+        return False
+
+    def available_moves(self):
+        move = []
+        for i in range(len(self.board)):
+            for j in range(i):
+                if self.board[i][j] == ',':
+                    move.append((i, j))
+        return move
+
+    def easy_bot_move(self):
+        """ assume there are always moves"""
+        moves = self.available_moves()
+        bot_choice = random.choice(moves)
+        self.board[bot_choice[0]][bot_choice[1]] = 'X' if self.player_turn % 2 == 0 else '0'
+
+
+class easy_bot:
+    def __int__(self, game: Tictactoe):
+        self.game = game
+    def start(self):
+        while not self.game.check_winner[0] and self.game.check_if_moves():
+            print('current board: \n ')
+            print(self.game.print_board(self.board))
+            l = move.split()
+            row = int(l[0])
+            column = int(l[1])
+            game.player_move(row, column)
+            print('you have made a move, here is the board now \n')
+
+            moves = self.game.available_moves()
+            while True:
+                if not moves:
+                    play = input('type 1 to play again, 0 to quit')
+                    while play != '1' or play != '0':
+                        play = input('type 1 to play again, 0 to quit')
+                    if play == '1':
+                        game.reset_board()
+                    else:
+                        game.end_game()
+                        break
+            #bot move
+
+
+
+
+
 
 def main():
     while True:
-        player_or_bot = input('type 1 to play against a friend or 2 for a bot: ')
+        player_or_bot = input('type 1 to play against a friend, 2 for a bot (easy mode), 3 for hard mode and 4 to watch two bots play ')
         if player_or_bot == '1':
             name1 = input("player1 name: ")
             name2 = input('player2 name: ')
             game = Tictactoe(name1, name2)
             while True:
+                if not game.available_moves():
+                    play = input('type 1 to play again, 0 to quit')
+                    while play != '1' or play != '0':
+                        play = input('type 1 to play again, 0 to quit')
+                    if play == '1':
+                        game.reset_board()
+                    else:
+                        game.end_game()
+                        break
                 print("here is the current board")
-                print(game.print_board(game.board))
+                print(game.print_board())
                 move = input("what row, column would you like to play on (row, column), (row and column are in between 0 and 2: ")
                 l = move.split()
                 row = int(l[0])
@@ -106,18 +164,17 @@ def main():
                     print('current score:')
                     print(f'player 1 has {game.player_1[1]} points')
                     print(f'player 2 has {game.player_2[1]} points')
-                    t = input('type 1 to play again, 2 to exit player vs player mode or 3 to reset points')
+                    t = input('type 1 to play again, 2 to end game or 3 to reset points and play again')
                     if t == '1':
                         game.new_game()
-                        continue
                     elif t == '2':
+                        game.end_game()
                         break
                     elif t == '3':
                         game.new_game_with_points_reset()
 
-
         if player_or_bot == '2':
-            pass
+
 
 
 
